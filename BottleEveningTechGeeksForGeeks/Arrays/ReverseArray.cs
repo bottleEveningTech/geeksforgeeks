e56c9c26761760761f40e778624664f722b9181c
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-06 01:12:33 +0530

    Reverse Array Problem Solved

---
+++ BottleEveningTechGeeksForGeeks/Arrays/ReverseArray.cs
@@ -0,0 +1,19 @@
+﻿public partial class Solution
+{
+    public void reverseArray(int[] arr)
+    {
+
+        int start = 0;
+        int end = arr.Length - 1;
+
+        while(start < end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
+}
