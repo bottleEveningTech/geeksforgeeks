610d095b9e3ab6a15c6eaf33558b4be3654c3bab
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-06 00:59:05 +0530

    Second Largest Problem Solved

---
+++ BottleEveningTechGeeksForGeeks/Arrays/SecondLargest.cs
@@ -0,0 +1,24 @@
+﻿public partial class Solution
+{
+    public int GetSecondLargest(int[] arr)
+    {
+        int max = -1;
+        int secondMax = -1;
+        int size = arr.Length;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] > max)
+            {
+                secondMax = max;
+                max = arr[i];
+            }
+            else if (arr[i] > secondMax && arr[i] != max)
+            {
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+
+    }
+}
