95d99c53e9d098630946ea8c8ba95e3132cbdc29
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-06 18:15:41 +0530

    Floor Element Sorted Array Problem Solved

---
+++ BottleEveningTechGeeksForGeeks/Binary Search/FloorElementSortedArray.cs
@@ -0,0 +1,31 @@
+﻿public partial class Solution
+{
+    public int FindFloor(int[] arr, int x)
+    {
+        // greatest element smaller than or equal to x
+
+        int start = 0;
+        int end = arr.Length - 1;
+        int result = -1;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == x)
+            {
+                result = mid;
+                start = mid + 1;
+            }
+            else if (arr[mid] < x)
+            {
+                result = mid;
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return result;
+    }
+}
