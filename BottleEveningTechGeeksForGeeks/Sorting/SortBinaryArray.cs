d24147e2fa8e62fe367f3c504d0e4fcacd012c2a
bottleEveningTech <tanishkasaxena2408@gmail.com>
2025-09-07 15:46:26 +0530

    Sort Binary Array Problem Solved

---
+++ BottleEveningTechGeeksForGeeks/Sorting/SortBinaryArray.cs
@@ -0,0 +1,37 @@
+﻿public partial class Solution
+{
+    public void sort012(int[] arr)
+    {
+        // using DNF
+        int low = 0;
+        int mid = 0;
+        int high = arr.Length - 1;
+
+        while (mid <= high)
+        {
+            if (arr[mid] == 0)
+            {
+                SwapElements(arr, low, mid);
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                (arr[mid], arr[high]) = (arr[high], arr[mid]);
+                high--;
+            }
+        }
+
+    }
+
+    private static void SwapElements(int[] arr, int e1, int e2)
+    {
+        int temp = arr[e1];
+        arr[e1] = arr[e2];
+        arr[e2] = temp;
+    }
+}
